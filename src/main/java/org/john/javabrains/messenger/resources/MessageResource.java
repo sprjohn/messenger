cc791b510af84c0796eedc9df22f0230a8758cf4
john <john.sprink@gmail.com>
2016-05-12 18:54:56 -0400

    Pulled @Produces and @Consumes annotations up to class level to simplify

--- src/main/java/org/john/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/john/javabrains/messenger/resources/MessageResource.java
@@ -8,27 +8,24 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
     MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
         return messageService.getAllMessages();
     }
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Message addMessage(Message message) {
         return messageService.addMessage(message);
     }
 
     @PUT
     @Path("/{messageId}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
         message.setId(messageId);
         return messageService.updateMessage(message);
@@ -36,14 +33,12 @@ public class MessageResource {
 
     @DELETE
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public void deleteMessage(@PathParam("messageId") long messageId) {
         messageService.removeMessage(messageId);
     }
 
     @GET
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Message getMessage(@PathParam("messageId") long messageId) {
         return messageService.getMessage(messageId);
     }
