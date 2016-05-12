614fc9ae1a1c97b059b9fc732b92650aad3ec341
john <john.sprink@gmail.com>
2016-05-12 18:49:40 -0400

    Added delete api

--- src/main/java/org/john/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/john/javabrains/messenger/resources/MessageResource.java
@@ -3,10 +3,7 @@ package org.john.javabrains.messenger.resources;
 import org.john.javabrains.messenger.model.Message;
 import org.john.javabrains.messenger.service.MessageService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -19,6 +16,29 @@ public class MessageResource {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
         return messageService.getAllMessages();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message addMessage(Message message) {
+        return messageService.addMessage(message);
+    }
+
+    @PUT
+    @Path("/{messageId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+        message.setId(messageId);
+        return messageService.updateMessage(message);
+    }
+
+    @DELETE
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void deleteMessage(@PathParam("messageId") long messageId) {
+        messageService.removeMessage(messageId);
     }
 
     @GET
