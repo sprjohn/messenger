72b37b90e1d113d00cdc0663239c8d088b685b8d
john <john.sprink@gmail.com>
2016-05-12 18:03:50 -0400

    Added json support

--- src/main/java/org/john/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/john/javabrains/messenger/resources/MessageResource.java
@@ -16,14 +16,14 @@ public class MessageResource {
     MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
         return messageService.getAllMessages();
     }
 
     @GET
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_JSON)
     public Message getMessage(@PathParam("messageId") long messageId) {
         return messageService.getMessage(messageId);
     }
