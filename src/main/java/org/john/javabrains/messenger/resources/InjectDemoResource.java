b60d2ccbf894adfce804669865d91671afb08388
john <john.sprink@gmail.com>
2016-05-13 14:40:11 -0400

    Added context retrieval example

--- src/main/java/org/john/javabrains/messenger/resources/InjectDemoResource.java
+++ src/main/java/org/john/javabrains/messenger/resources/InjectDemoResource.java
@@ -1,6 +1,9 @@
 package org.john.javabrains.messenger.resources;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/injectdemo")
 public class InjectDemoResource {
@@ -10,5 +13,14 @@ public class InjectDemoResource {
                                             @HeaderParam("authSessionId") String header,
                                             @CookieParam("name") String cookie) {
         return "Matrix param: " + matrixParam + "\nHeader param: " + header + "\nCookie value: " + cookie;
+    }
+
+    @GET
+    @Path("context")
+    public String getParamsUsingContext(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders) {
+        String path = uriInfo.getAbsolutePath().toString();
+        String cookies = httpHeaders.getCookies().toString();
+
+        return "Path: " + path + "\nCookies: " + cookies;
     }
 }
