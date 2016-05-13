33b4a28d24d912582c811c393deabb0485937aad
john <john.sprink@gmail.com>
2016-05-12 20:21:41 -0400

    Added backing message service call to support filtering and pagination

--- src/main/java/org/john/javabrains/messenger/service/MessageService.java
+++ src/main/java/org/john/javabrains/messenger/service/MessageService.java
@@ -4,14 +4,32 @@ import org.john.javabrains.messenger.database.DummyDatabase;
 import org.john.javabrains.messenger.model.Message;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MessageService {
     private Map<Long, Message> messages = DummyDatabase.getMessages();
 
     public List<Message> getAllMessages() {
         return new ArrayList<>(messages.values());
+    }
+
+    public List<Message> getMessagesForYear(int year) {
+        return messages.values().stream()
+                .filter(m -> m.getCreated().getYear() == year)
+                .collect(Collectors.toList());
+    }
+
+    public List<Message> getAllMessagePaginated(int start, int size) {
+        List<Message> list = new ArrayList<>(messages.values());
+
+        if (start + size > list.size()) {
+            return Collections.emptyList();
+        }
+
+        return list.subList(start, start + size);
     }
 
     public Message getMessage(long id) {
